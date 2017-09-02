a0c70d53e949edde3c6fdc159130b267f2b81b13
cheyu0209 <cheyu0209@gmail.com>
2017-09-02 21:53:05 +0800

    1. Two Sum

---
+++ JavaScript/1.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+
+    var map=[];
+    var tmp;
+
+    /* 將 nums 與 map 的 index 與 value 相互對應 */
+    for(i=0; i<nums.length;i++)
+        map[ nums[i] ] = i;
+
+    /* target-nums 如果存在，則 map 有對應的值 */
+    for(i=0; i<nums.length-1; i++)
+    {
+        tmp = target-nums[i];
+        // 確認其值存在且非原值
+        if(map[tmp] && (map[tmp] != i))
+            return [i, map[tmp]];
+    }
+
+};
