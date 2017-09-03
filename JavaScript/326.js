68bda7f839a6a8abe10d68010eded13a04b7c602
cheyu0209 <cheyu0209@gmail.com>
2017-09-03 12:55:33 +0800

    326. Power of Three

--- JavaScript/326.js
+++ JavaScript/326.js
@@ -4,10 +4,22 @@
  */
 var isPowerOfThree = function(n) {
 
+   /* 換底公式 */
+   /*
     if( ( Math.log10(n)/Math.log10(3) ) % 1 === 0 )
         return true;
     else
         return false;
+    */
+
+    /* 遞迴 */
+    if(!n)
+      return false;
+    if(n === 1)
+      return true;
+    if((n%3) !== 0)
+      return false;
+    return isPowerOfThree(n/3);
 
 };
 
